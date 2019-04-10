4692ff4c9f48b73b0c5279a53008a619bbea9938
Nunes <marcosrenatovnunes@hotmail.com>
2019-04-10 16:39:37 -0300

    Storybook to be used on the future

---
+++ web/libs/stories/index.stories.js
@@ -0,0 +1,26 @@
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react';
+import { Button, Welcome } from '@storybook/react/demo';
+import React from 'react';
+import HelloWorld from '../../components';
+
+
+
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+))
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+      <span role='img' aria-label='so cool'>
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ))
+
+storiesOf('HelloWorld', module).add('simple component', () => <HelloWorld />)
--- web/pages/index.js
+++ web/pages/index.js
@@ -1,38 +1,9 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import styled from 'styled-components';
-import { createGrow } from '../ducks/growBoard';
 
-const Title = styled.h1`
-color: red;
-background: blue;
-`;
 class index extends Component {
-  handleIncrease = () => {
-    const { createGrow } = this.props;
-    createGrow('dasdsa');
-  }
-
   render() {
-    console.log('props', this.props);
-    const { counter } = this.props;
-    return (
-      <div>
-        <div>Counter: {counter}</div>
-        <button onClick={this.handleIncrease}>Increase</button>
-      </div>
-    )
+    return <div>JIO</div>;
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    counter: state.growBoardReducer.counter
-  }
-}
-
-const mapDispatchToProps = {
-  createGrow
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(index);
+export default index;
