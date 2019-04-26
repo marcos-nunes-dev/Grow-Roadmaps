ad5af7a4d5ef5fc0c3ccfd21844f771acec05902
Marcos Renato <marcosrenatovnunes@hotmail.com>
2019-04-25 22:11:52 -0300

    Convert to Hooks

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -4,20 +4,23 @@ import { ApolloProvider } from 'react-apollo';
 import { Provider } from 'react-redux';
 import withApolloClient from '../libs/with-apollo-client';
 import withReduxStore from '../libs/with-redux-store';
+import { StoreContext } from 'redux-react-hook';
 
 class MyApp extends App {
-  render () {
-    const {Component, pageProps, reduxStore, apolloClient} = this.props
+  render() {
+    const { Component, pageProps, reduxStore, apolloClient } = this.props;
     return (
       <Container>
         <ApolloProvider client={apolloClient}>
           <Provider store={reduxStore}>
-            <Component {...pageProps} />
+            <StoreContext.Provider value={reduxStore}>
+              <Component {...pageProps} />
+            </StoreContext.Provider>
           </Provider>
         </ApolloProvider>
       </Container>
-    )
+    );
   }
 }
 
-export default withApolloClient(withReduxStore(MyApp))
+export default withApolloClient(withReduxStore(MyApp));
--- web/pages/index.js
+++ web/pages/index.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Wrapper from '../components/Common/DefaultWrapper';
 import Content from '../components/MainScene/Content';
 import Modules from '../components/MainScene/Modules';
 import Roadmaps from '../components/MainScene/Roadmaps';
 
-class index extends Component {
-  render() {
-    return (
-      <Wrapper>
-        <Roadmaps />
-        <Modules />
-        <Content />
-      </Wrapper>
-    );
-  }
+export default function index() {
+  return (
+    <Wrapper>
+      <Roadmaps />
+      <Modules />
+      <Content />
+    </Wrapper>
+  );
 }
-
-export default index;
