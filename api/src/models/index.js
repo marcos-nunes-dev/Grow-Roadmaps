2357ee3ca2c3afce550402da83706aafd281862f
Claudson Oliveira <claudsonweb@gmail.com>
2019-05-04 18:55:34 +0100

    Remove config por ambiente
    Respeitando o 12factor [1] a ideia é que um ambiente
    seja definido apenas como um conjunto de variaveis de ambiente,
    nada no código fala sobre ambientes, fazer isso causa problemas quando
    queremos adicionar um novo.
    
    [1]: https://12factor.net/config

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -5,21 +5,16 @@ var path = require('path');
 var Sequelize = require('sequelize');
 var basename = path.basename(__filename);
 var env = process.env.NODE_ENV || 'development';
-var config = require(__dirname + '/../config/sequelize.config.js')[env];
+var config = require(__dirname + '/../config/sequelize.config.js');
 var db = {};
 
-var sequelize = null;
+var sequelize  = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
 
 fs.readdirSync(__dirname)
   .filter(file => {
